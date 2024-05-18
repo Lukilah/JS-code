f90d84a5250df9bab285f0d47304bb2e03e25dc3
Lukilah <111893154+Lukilah@users.noreply.github.com>
2024-05-18 23:47:35 +0300

    Used several npm libraries for this
    Namely sillyName & randomSuperHero

---
+++ index.js
@@ -0,0 +1,12 @@
+//var generateName = require('sillyname');
+
+// import generateName from "sillyname";
+// var sillyName = generateName();
+
+// console.log(`My name is ${sillyName}.`);
+
+//random superhero name
+import {randomSuperhero} from 'superheroes';
+const superheroName = randomSuperhero();
+
+console.log(`I am: ${superheroName}!`);
